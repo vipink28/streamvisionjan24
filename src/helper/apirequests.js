f84b35cc509e304a943c5140f14ca65fdbf09d66
vipink28 <vipinkalyan28@gmail.com>
2024-02-16 11:03:04 +0530

    project initialised

---
+++ src/helper/apirequests.js
@@ -0,0 +1,5 @@
+const API_KEY = '0d8ab7cff2692bd014bb25fca16d7158';
+
+export const requests = {
+    getNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213&language=en-US`
+}
---
+++ src/pages/Homescreen.jsx
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import React from 'react';
+import { requests } from '../helper/apirequests';
+
+function Homescreen(props) {
+    return (
+        <div>
+            Homescreen
+        </div>
+    );
+}
+
+export default Homescreen;
