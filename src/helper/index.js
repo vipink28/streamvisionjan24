8ef34b328889422a2ac429f8d176a592b20c2052
vipink28 <vipinkalyan28@gmail.com>
2024-03-02 10:21:02 +0530

    shuffle array added

--- src/helper/index.js
+++ src/helper/index.js
@@ -12,3 +12,22 @@ export const showYear = (dateString) => {
     let myDate = new Date(dateString);
     return myDate.getFullYear();
 }
+
+
+export const shuffle = (array) => {
+    let currentIndex = array.length, randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex !== 0) {
+
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+}
